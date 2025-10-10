d52abd6380765b2b8e070be5bbb47357a43fc2b5
Sachin <mrsachinsehrawat07@gmail.com>
2025-10-10 14:23:08 +0530

    update devTools

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -6,7 +6,7 @@ const store = configureStore({
   reducer: {
     themeReducer: themeReducer
   },
-  // devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
